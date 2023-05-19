5f5287345ec428ff813dcbea56450fa79eadf1ff
mehadi-shuvo <mehadihasanshuvo88@gamil.com>
2023-05-19 20:57:12 +0600

    data load for one product

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 3000;
@@ -36,6 +36,13 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/category/:id', async (req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await shopCategoryCollection.findOne(query);
+      res.send(result)
+
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
