559385a320cbb140e647cd10eacf5dd379e79d93
mehadi-shuvo <mehadihasanshuvo88@gamil.com>
2023-05-20 16:31:23 +0600

    post function create

--- index.js
+++ index.js
@@ -54,6 +54,15 @@ async function run() {
       res.send(result)
 
     })
+
+    app.post('/toys', async(req, res)=>{
+      const newToy = req.body;
+      const result = await shopCategoryCollection.insertOne(newToy);
+      res.send(result)
+  })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
