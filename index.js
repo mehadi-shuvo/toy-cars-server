494a911aef7f57fe7eec5a62b3ad30eeb3da65ec
mehadi-shuvo <mehadihasanshuvo88@gamil.com>
2023-05-20 21:59:44 +0600

    added delete function

--- index.js
+++ index.js
@@ -86,6 +86,13 @@ async function run() {
 
   })
 
+  app.delete('/toys/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) }
+    const result = await shopCategoryCollection.deleteOne(query)
+    res.send(result)
+  })
+
 
 
     // Send a ping to confirm a successful connection
